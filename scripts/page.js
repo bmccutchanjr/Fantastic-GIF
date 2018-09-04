bcf65e898a1da9e5d16df472843a37d2c8b5c295
Bill McCutchan <bmccutchanjr@gmail.com>
2018-09-04 16:18:44 -0400

    fixed event handlers

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,8 @@
 var buttons = [];           // array of buttons
 var imageURLs = [];         // array of objects containing the URL for still and animated images
+var GIPHYoffset = 0;        // first image to tetrieve from GIPHY
 const stillImage = 0;       // index to the still image in imageURLs
-const runImage = 1;     // index to the running image in imageURLs
+const runImage = 1;         // index to the running image in imageURLs
 
 var myAPIKey = "3AFkSx6nd8NPugiU5xJtbRdNWYAeS83c";
 
@@ -143,6 +144,18 @@ function addButton()
     localStorage.setItem ("buttons", buttons);
 }
 
+function formatDate (date)
+{   // convert the date to a friendlier format
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    var formatted = months[date.substring(5, 7) - 1] + " " +
+                    date.substring(8, 10) + ", " +
+                    date.substring(0, 4);
+
+    return formatted;
+}
+
 function getBandsInTown (artist)
 {   // interface with BandsInTown.com to get data for the band.  I'm thinking websites and upcoming
     // tour dates.
@@ -155,11 +168,12 @@ function getBandsInTown (artist)
     .then (function(response)
     {   //
 
-        var newLink = $("<a>");
-        var newDiv = $("<div>");
-
         if (response.facebook_page_url)
-        {   newLink
+        {   // The artist has a FaceBook page...and a link
+            
+            var newLink = $("<a>");
+
+            newLink
                 .addClass("something")
                 .attr("href", response.facebook_page_url)
                 .text(artist + " on FaceBook");
@@ -168,7 +182,9 @@ function getBandsInTown (artist)
         }
 
         if (response.upcoming_event_count > 0)
-        {   // If there are no upcoming events, don't bother looking for any...
+        {   // The artist has upcoming events (concerts?)...get the dates and display them
+
+            var newDiv = $("<div>");
 
             URL = "https://rest.bandsintown.com/artists/" +
                 artist +
@@ -178,27 +194,59 @@ function getBandsInTown (artist)
             .then (function(response)
             {   // display upcoming tour dates for Akron, Cleveland and Youngstown
 
+                // first, clear any data that might be in #concert-div from another query
+                $("#concert-div").empty();
+
+                var dateInRegion = false;
+                var divTag = $("<div>");
+
                 response.forEach(function(concert)
                 {   if ((concert.venue.country) &&
                         (concert.venue.country === "United States"))
-                    {   
-                        if ((concert.venue.city === "Akron") ||
-                            (concert.venue.city === "Buffalo") ||
-                            (concert.venue.city === "Cleveland") ||
-                            (concert.venue.city === "Pittsburg") ||
-                            (concert.venue.city === "Youngstown"))
-                        {   var newP = $("<p>");
-                            newP
-                                .text(artist + " is coming to " + concert.venue.city +
-//                                 " on " + concert.datetime.substring(9));
-                                " on " + concert.datetime);
+                    {   // I'm only interested in concerts in Ohio and the surrounding states
+                        //
+                        // Bands In Town gives latitude and longitude for the venue, so it should
+                        // be possible to calculate the distance from the users location to the
+                        // venue, which would make the page more useable (ans may be even more accurate)
+                        // -- but that's beyond the scope of this project.
 
-                            $("#image-main")
-                                .prepend($("<hr>"))
-                                .prepend(newP);
+                        var region = ["IN", "KY", "MI", "NY", "OH", "PA", "WV"];
+
+                        if (region.indexOf (concert.venue.region) != -1)
+                        {   dateInRegion = true;
+
+                            var pTag1 = $("<p>");
+                            pTag1
+                                .text(concert.venue.city +
+                                      " on " + formatDate (concert.datetime)); 
+
+                            divTag
+                                .append (pTag1);
                         }
                     }
                 })
+
+                if (dateInRegion)
+                {   var h2Tag = $("<h2>");
+                    h2Tag.text(artist + " will be appearing in:");
+
+                    $("#concert-div")
+                        .prepend(h2Tag)
+                        .append(divTag);
+
+                    // Concert data is in a <div> that is hidden by default.  Add a button to the
+                    // page to tell the user there are concert dates to view and lets them view those
+                    // dates.
+
+                    var cButton = $("<button>");
+
+                    cButton
+                        .addClass("something")
+                        .attr("value", "concert")
+                        .text("UPCOMING CONCERTS");
+
+                    $("#image-main").prepend (cButton);
+                }
             })
         }
     })
@@ -211,18 +259,16 @@ function getBandsInTown (artist)
 function getGIPHY (getWhat)
 {   // interface with GIPHY to retrieve images
 
-    // This is the event handler for the topic buttons.  That means the user clicked on one of the
-    // buttons located in .button-div.  Whether this button is the same as the one just previously
-    // clicked or not, it's okay to clear anything currently in #image-div
-    
-    $("#image-main").empty();
-    imageURLs = [];
+    // remove the GET MORE button.  The GET MORE button is the last thing added to #image-main by this
+    // function and should be the last child.  The only time GET MORE is not the last child is when
+    // #image-main is empty.
+
+    $("#image-main:last-child").remove();
 
     // create the search string
-    var URL = "http://api.giphy.com/v1/gifs/search?q=" + 
-        $(getWhat).attr("value") +
+    var URL = "http://api.giphy.com/v1/gifs/search?q=" + getWhat +
         "&api_key=" + myAPIKey +
-        "&limit=10";
+        "&limit=10&offset=" + GIPHYoffset;
 
     // and search GIPHY
     $.get(URL)
@@ -258,8 +304,20 @@ function getGIPHY (getWhat)
             $("#image-main").append(newDiv);
         })
 
-        // and now, get info on the band...
-        getBandsInTown ($(getWhat).text());
+        // add a button to get 10 more GIFs
+        var button = $("<button>");
+        button
+            .addClass ("10-more")
+            .attr("value", getWhat)
+            .text("GET 10 MORE");
+
+        $("#image-main")
+            .append(button);
+
+        // and now, get info on the band...but only do it once
+
+        if (GIPHYoffset === 0)
+            getBandsInTown (getWhat);
     })
     .catch(function()
     {   // some kind of error occured...can't display any images
@@ -278,12 +336,29 @@ $(document).ready(function()
 
     $("#add-button").click(addButton);
 
-    $(".button-div button").click(function()
+    $(".button-div").on ("click", "button", function()
     {   // generic event handler for topic buttons
-        getGIPHY (this);
+
+        // This is the event handler for the topic buttons.  That means the user clicked on one of the
+        // buttons located in .button-div.  Whether this button is the same as the one just previously
+        // clicked or not, it's okay to clear anything currently displayed in #image-div or #concert-div
+    
+        $("#image-main").empty();
+        $("#concert-div").empty();
+        imageURLs = [];
+        GIPHYoffset = 0;
+
+        // and get data for the button that was clicked
+        getGIPHY ($(this).text());
     });
 
     $("#image-main")
+        .on("click", ".10-more", function()
+        {   // generic event handler for topic buttons
+
+            GIPHYoffset += 10;
+            getGIPHY ($(this).attr("value"));
+        })
         .on("mouseenter", ".image", function()
         {   // It appears jQuery does not support a single method for "hover", but rather uses
             // .mouseenter() end .mouseleave() events.  (Actually jQuery does have a .hover()
